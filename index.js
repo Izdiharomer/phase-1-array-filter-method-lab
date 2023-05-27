7b7f90efcfb7bd95069b33b6efef3e593630118f
Izdihar <ezdiharomerrrr@gmail.com>
2023-05-27 20:57:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+
+
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+ 
+  function findMatching(drivers, name) {
+  const lowercaseName = name.toLowerCase();
+  return drivers.filter(driver => driver.toLowerCase() === lowercaseName);
+}
+
+  function fuzzyMatch(drivers, letters) {
+    const pattern = new RegExp(`^${letters}`, 'i');
+    return drivers.filter(driver => pattern.test(driver));
+  }
+  findMatching()
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+
+
+
